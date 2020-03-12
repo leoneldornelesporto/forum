f401bd37eb98713c0edf6987287d98e637e3706d
Leonel Dorneles Porto <leoneldornelesporto@Outlook.com>
2020-03-12 17:54:55 -0300

    Realizando modificações

--- src/main/java/br/com/forum/alura/AluraApplication.java
+++ src/main/java/br/com/forum/alura/AluraApplication.java
@@ -2,8 +2,12 @@ package br.com.forum.alura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport //Para habilitar paginação
+@EnableCaching //Usa cache na memoria
 public class AluraApplication {
 
     public static void main(String[] args) {
--- src/main/java/br/com/forum/alura/endPoint/RespostaEndPoint.java
+++ src/main/java/br/com/forum/alura/endPoint/RespostaEndPoint.java
@@ -1,12 +1,12 @@
 package br.com.forum.alura.endPoint;
 
+import br.com.forum.alura.dto.RespostaAtualizaoForm;
 import br.com.forum.alura.dto.RespostaDto;
 import br.com.forum.alura.dto.RespostaForm;
 import br.com.forum.alura.model.Resposta;
 import br.com.forum.alura.model.Topico;
 import br.com.forum.alura.repository.RespostaRepository;
 import br.com.forum.alura.repository.TopicoRepository;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,9 +44,45 @@ public class RespostaEndPoint {
                     ).build().toUri()
             ).body(new RespostaDto(resposta));
         }
-        else
-        {
+        else {
             return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> update(@RequestBody RespostaAtualizaoForm respostaAtualizaoForm, @PathVariable Long id){
+        Optional<Topico> topico = topicoRepository.findById(id);
+
+        if(topico.isPresent()){
+            Resposta resposta = new Resposta(respostaAtualizaoForm.getMensagem(),topico.get());
+            resposta.setId(respostaAtualizaoForm.getRespostaId());
+            respostaRepository.save(resposta);
+
+            return new ResponseEntity<>(respostaAtualizaoForm,HttpStatus.CREATED);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        if (respostaRepository.findById(id).isPresent()){
+            respostaRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllRespostas(){
+        if(!respostaRepository.findAll().isEmpty()){
+            return new ResponseEntity<>(RespostaDto.converter(respostaRepository.findAll()),HttpStatus.OK);
+        }
+        else{
+            return ResponseEntity.notFound().build();
         }
     }
 
