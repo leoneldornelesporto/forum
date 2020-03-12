bdcaa1d232920139c684b7857b955203be25ca36
Leonel Dorneles Porto <leoneldornelesporto@Outlook.com>
2020-03-12 17:51:07 -0300

    Modificando o arquivo UsuarioRepository.java, incluindo novas pesquisas no banco de dados.

--- src/main/java/br/com/forum/alura/repository/UsuarioRepository.java
+++ src/main/java/br/com/forum/alura/repository/UsuarioRepository.java
@@ -1,7 +1,16 @@
 package br.com.forum.alura.repository;
 
 import br.com.forum.alura.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    Page<Usuario> findByNome(String nome, Pageable paginacao);
+    Optional<Usuario> findByEmail(String email);
 }
