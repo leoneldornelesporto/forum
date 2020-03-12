9aaa17428785fdf0825154959c549a6e1528f090
Leonel Dorneles Porto <leoneldornelesporto@Outlook.com>
2020-03-12 17:49:02 -0300

    Modificando o arquivo UsuarioEndPoint.java

--- src/main/java/br/com/forum/alura/endPoint/UsuarioEndPoint.java
+++ src/main/java/br/com/forum/alura/endPoint/UsuarioEndPoint.java
@@ -1,18 +1,22 @@
 package br.com.forum.alura.endPoint;
 
-import br.com.forum.alura.dto.TopicoDto;
-import br.com.forum.alura.model.Curso;
-import br.com.forum.alura.model.Topico;
+import br.com.forum.alura.dto.UsuarioAtualizacaoForm;
+import br.com.forum.alura.dto.UsuarioDto;
+import br.com.forum.alura.model.Usuario;
 import br.com.forum.alura.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import javax.transaction.Transactional;
 
 @RestController
 @RequestMapping(path = "/usuario")
@@ -25,12 +29,50 @@ public class UsuarioEndPoint {
     }
 
     @GetMapping
-    public ResponseEntity<Object> getAllUsuarios(){
-        if(usuarioRepository.findAll().isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @Cacheable(value = "getAllUsuarios")
+    public Page<UsuarioDto> getAllUsuarios(@RequestParam (required = false) String nome, @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao) {
+        if(nome == null){
+            return UsuarioDto.converter(usuarioRepository.findAll(paginacao));
         }
         else {
-            return new ResponseEntity<>(usuarioRepository.findAll(),HttpStatus.OK);
+            return UsuarioDto.converter(usuarioRepository.findByNome(nome,paginacao));
+        }
+    }
+
+    @PostMapping
+    @Transactional
+    @CacheEvict(value = "getAllUsuarios", allEntries = true)
+    public ResponseEntity<?> create(@RequestBody Usuario usuario){
+        if(!(usuario == null)){
+            usuarioRepository.save(usuario);
+            return ResponseEntity.created(MvcUriComponentsBuilder.fromMethodName(UsuarioEndPoint.class,"create",usuario.getId()).build().toUri()).body(new UsuarioDto(usuario));
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    @CacheEvict(value = "getAllUsuarios", allEntries = true)
+    public ResponseEntity<?> update(@RequestBody UsuarioAtualizacaoForm usuarioAtualizacaoForm, @PathVariable Long id){
+        if(usuarioRepository.findById(id).isPresent()){
+            Usuario usuario = usuarioAtualizacaoForm.formUpdate(usuarioRepository,id);
+            return new ResponseEntity<>(new UsuarioDto(usuario),HttpStatus.OK);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    @CacheEvict(value = "getAllUsuarios", allEntries = true)
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        if (usuarioRepository.findById(id).isPresent()){
+            usuarioRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
