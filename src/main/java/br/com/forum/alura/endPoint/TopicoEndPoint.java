d5867fae37556f15601f7a2242f98e27cf1b490e
Leonel Dorneles Porto <leoneldornelesporto@Outlook.com>
2020-03-12 17:48:12 -0300

    Modificando o arquivo TopicoEndPoint.java

--- src/main/java/br/com/forum/alura/endPoint/TopicoEndPoint.java
+++ src/main/java/br/com/forum/alura/endPoint/TopicoEndPoint.java
@@ -1,20 +1,17 @@
 package br.com.forum.alura.endPoint;
 
-import br.com.forum.alura.dto.AtualizacaoTopicoForm;
-import br.com.forum.alura.dto.TopicoDetalheDto;
-import br.com.forum.alura.dto.TopicoDto;
-import br.com.forum.alura.dto.TopicoForm;
+import br.com.forum.alura.dto.*;
 import br.com.forum.alura.model.Topico;
 import br.com.forum.alura.repository.CursoRepository;
+import br.com.forum.alura.repository.RespostaRepository;
 import br.com.forum.alura.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.persistence.Entity;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
@@ -25,11 +22,13 @@ import java.util.Optional;
 public class TopicoEndPoint {
     private TopicoRepository topicoRepository;
     private CursoRepository cursoRepository;
+    private RespostaRepository respostaRepository;
 
     @Autowired
-    public TopicoEndPoint(TopicoRepository topicoRepository, CursoRepository cursoRepository) {
+    public TopicoEndPoint(TopicoRepository topicoRepository, CursoRepository cursoRepository, RespostaRepository respostaRepository) {
         this.topicoRepository = topicoRepository;
         this.cursoRepository = cursoRepository;
+        this.respostaRepository = respostaRepository;
     }
 
     @GetMapping
@@ -96,5 +95,17 @@ public class TopicoEndPoint {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/resposta/{id}")
+    @Transactional
+    public ResponseEntity<?> deleteResposta(@PathVariable Long id){
+        if(respostaRepository.findById(id).isPresent()){
+            respostaRepository.deleteById(id);
+            return new ResponseEntity<>(id,HttpStatus.OK);
+        }
+        else{
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
